e4ea4f1f5f4028e12866614cd3e6709dfbc2895b
Diodoru <newlolo510@gmail.com>
2026-04-07 22:26:12 +0200

    add sujet et reponse function

--- db/database.py
+++ db/database.py
@@ -3,6 +3,9 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 from dotenv import load_dotenv
 
+from models.sujet import Sujet
+from models.reponse import Reponse
+
 dotenv_file = ".env.local"
 load_dotenv(dotenv_file)
 url = os.getenv("DATABASE_URL")
@@ -15,3 +18,19 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Base déclarative pour les modèles
 Base = declarative_base()
+
+def add_sujet(sujet: Sujet):
+    session = SessionLocal()
+    try:
+        session.add(sujet)
+        session.commit()
+    finally:
+        session.close()
+
+def add_reponse(reponse: Reponse):
+    session = SessionLocal()
+    try:
+        session.add(reponse)
+        session.commit()
+    finally:
+        session.close()
