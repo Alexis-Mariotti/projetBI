6b4f4bc505942d76a5fc4aef4a916dbf8ad4c1d4
Diodoru <newlolo510@gmail.com>
2026-04-08 13:44:09 +0200

    add symbole boursier dans la table action

--- models/action.py
+++ models/action.py
@@ -8,6 +8,7 @@ class Action(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     nom_action = Column(String(255), nullable=True)
+    symbole_boursier = Column(String(255), nullable=True)
 
     secteur = Column(Integer, ForeignKey("secteur.id"))
     indice_reference = Column(Integer, ForeignKey("indice_reference.id"))
